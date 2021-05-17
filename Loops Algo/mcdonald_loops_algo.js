b7c314024cee0b03cba3bd98a5cd04ddef4749b8
mamcdonald94 <mamcdonald94@gmail.com>
2021-05-17 11:02:26 -0400

    inital commit

---
+++ Loops Algo/mcdonald_loops_algo.js
@@ -0,0 +1,21 @@
+// print all odd numbers from 1 to 20: //
+
+for (var x = 1; x <= 20; x += 2){
+    console.log(x)
+}
+
+// sum and print numbers 1-5: //
+
+for (var x = 1; x <= 5; x++){
+    var sum = x * ((x + 1)/ 2)
+    if (x == 1){
+        console.log("Num: ", x)
+        console.log("Sum: ", x)
+    }
+    else {
+        console.log("Num: ", x)
+        console.log("Sum: ", sum)
+    }
+}
+
+
